8ce90e6ac3c6a650e35ec08c86ddbe0dbe550f21
Maxim <mdolgov322@gmail.com>
2022-07-23 01:14:01 +0400

    add config.py to commit

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = 0 # here your api_id
+api_hash = '0' # here your api_hash
+
+strict_filter = ['TEST'] # strict filters
+lax_filter = ['te', 'st'] # lax filters in lowercase
+
+usernames = ['fisrt_username', 'second_username'] # username the script to work with chats
+username_exception = ['my_username'] # username the script not to work with chats (often your username)
